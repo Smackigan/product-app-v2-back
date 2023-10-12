08c889ecf694f586e219c65f2a1bd5c8f2217fd1
Paul <pablovanduo@gmail.com>
2023-10-12 11:22:17 +0300

    Adjust API

--- api/index.php
+++ api/index.php
@@ -12,69 +12,98 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
 
-// Define the API endpoint
-// GET PRODUCTS
+// API ENDPOINTS
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['endpoint'])) {
     $endpoint = $_GET['endpoint'];
 
     if ($endpoint === '/api/get-product') {
-        // Create an instance of the ProductsTable class
-        $productsTable = new ProductsTable();
-
-        // Use the getAllProducts function to retrieve product data
-        $products = $productsTable->getAllProducts();
-
-        // Return the product data as JSON
-        echo json_encode($products);
-        exit();
+        // GET PRODUCTS
+        handleGetProduct();
     }
-}
-
-
-// ADD PRODUCT
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_GET['endpoint'])) {
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_GET['endpoint'])) {
     $endpoint = $_GET['endpoint'];
-
     $jsonData = file_get_contents('php://input');
-    $data = json_decode($jsonData, true); // Convert JSON to an associative array
+    $data = json_decode($jsonData, true);
 
     if ($endpoint === '/api/add-product') {
-        // Handle the /api/add-product endpoint
-
-        $productController = new ProductController();
-        $response = $productController->createNewProduct($data);
-
-        // Send the response as JSON
-        if ($response) {
-            // Return the created product data as JSON
-            echo json_encode($response);
-        } else {
-            // Handle the case where the product creation failed
-            http_response_code(500); // Internal Server Error
-            echo json_encode($response);
-        }
-        exit();
+        // ADD PRODUCTS
+        handleAddProduct($data);
+    } elseif ($endpoint === '/api/delete-products') {
+        // DELETE PRODUCTS
+        handleDeleteProducts($data);
     }
 }
+
+// GET PRODUCTS
+function handleGetProduct()
+{
+    // Create an instance of the ProductsTable
+    $productsTable = new ProductsTable();
+
+    // Use getAllProducts function to retrieve product data
+    $products = $productsTable->getAllProducts();
+
+    // Return the product data as JSON
+    echo json_encode($products);
+    exit();
+}
+
+// ADD PRODUCTS
+function handleAddProduct($data)
+{
+    $productController = new ProductController();
+    $response = $productController->createNewProduct($data);
+
+    // Send the response as JSON
+    if ($response) {
+        // Return the created product data as JSON
+        echo json_encode($response);
+    } else {
+        // Product creation failed
+        http_response_code(500);
+        echo json_encode(['error' => 'Product creation failed']);
+    }
+    exit();
+}
+
+// DELETE PRODUCTS
+function handleDeleteProducts($data)
+{
+    // Create an instance of the DeleteProductsController
+    $deleteProductsController = new DeleteProductsController();
+
+    // Call the deleteProducts method to handle the deletion
+    $deleteProductsController->deleteProducts();
+}
+
+
+// if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_SERVER['REQUEST_URI'])) {
+//     $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+//     if ($urlPath === '/api/get-product') {
+//         // Handle the request here
+//     }
+
 
 // CHECK UNIQUE SKU
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action']) && $_GET['action'] === 'check-sku') {
-    // Check if the 'sku' parameter is set in the URL
-    if (isset($_GET['sku'])) {
-        // Get the SKU from the URL
-        $sku = $_GET['sku'];
+// if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action']) && $_GET['action'] === 'check-sku') {
+//     // Check if the 'sku' parameter is set in the URL
+//     if (isset($_GET['sku'])) {
+//         // Get the SKU from the URL
+//         $sku = $_GET['sku'];
 
-        // Create an instance of the ProductsTable class
-        $productsTable = new ProductsTable();
+//         // Create an instance of the ProductsTable class
+//         $productsTable = new ProductsTable();
 
-        // Use the isSkuUnique function to check if SKU is unique
-        $isUnique = $productsTable->isSkuUnique($sku);
+//         // Use the isSkuUnique function to check if SKU is unique
+//         $isUnique = $productsTable->isSkuUnique($sku);
 
-        // Return the result as JSON
-        echo json_encode($isUnique);
-        exit();
-    }
-}
+//         // Return the result as JSON
+//         echo json_encode($isUnique);
+//         exit();
+//     }
+// }
 
 // VALIDATION CHECK
 // if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action']) && $_GET['action'] === 'check-sku') {
@@ -96,19 +125,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action']) && $_GET['act
 //         echo json_encode($response);
 //     }
 // }
-
-// REMOVE PRODUCT
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_GET['endpoint'])) {
-    $endpoint = $_GET['endpoint'];
-
-    error_log("Received a request for endpoint: " . $endpoint);
-
-    if ($endpoint === '/api/delete-products') {
-
-        // Create an instance of the DeleteProductsController
-        $deleteProductsController = new DeleteProductsController();
-
-        // Call the deleteAllProducts method to handle the deletion
-        $deleteProductsController->deleteProducts();
-    }
-}
